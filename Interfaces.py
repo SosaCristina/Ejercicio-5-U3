f5405905bb233469017daad7e36e418efff1e2db
SosaCristina <103973005+SosaCristina@users.noreply.github.com>
2023-05-25 22:31:02 -0300

    Add files via upload

---
+++ Interfaces.py
@@ -0,0 +1,9 @@
+from zope.interface import Interface
+
+class interfaces(Interface):
+    def insertarElemento (self):
+        pass
+    def agregarElemento (self):
+        pass
+    def mostrarElemento (self):
+        pass
